c3d00fab2457bce9d590166a8a5eb1cf8679030f
pulkitbest <pulkitgupta38@gmail.com>
2023-07-21 16:46:55 +0530

    Bcrypt

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -33,14 +33,14 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
 };
 
 // Encrypt password using bcrypt
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) {
-    next();
-  }
+// userSchema.pre('save', async function (next) {
+//   if (!this.isModified('password')) {
+//     next();
+//   }
 
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-});
+//   const salt = await bcrypt.genSalt(10);
+//   this.password = await bcrypt.hash(this.password, salt);
+// });
 
 const User = mongoose.model('User', userSchema);
 
